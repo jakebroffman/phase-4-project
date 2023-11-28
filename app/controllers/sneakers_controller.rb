8dd53529c41e8306a7ca1409fcd734ab4f3acd7f
jakebroffman <jake.broffman1088@gmail.com>
2023-11-28 18:13:38 -0500

    build Sneaker controller actions

--- app/controllers/sneakers_controller.rb
+++ app/controllers/sneakers_controller.rb
@@ -1,2 +1,54 @@
 class SneakersController < ApplicationController
-end
+    before_action :set_sneaker, only: [:show, :edit, :update, :destroy]
+  
+    def index
+      @sneakers = Sneaker.all
+      render json: @sneakers
+    end
+  
+    def show
+      render json: @sneakers
+    end
+  
+    def new
+      @sneaker = Sneaker.new
+      render json: @sneaker
+    end
+  
+    def create
+      @sneaker = Sneaker.new(sneaker_params)
+      if @sneaker.save
+        render json: @sneaker, status: :created
+      else
+        render json: { errors: @sneaker.errors.full_messages }, status: :unprocessable_entity
+      end
+    end
+  
+    def edit
+      render json: @sneaker
+    end
+  
+    def update
+      if @sneaker.update(sneaker_params)
+        render json: @sneaker
+      else
+        render json: { errors: @sneaker.errors.full_messages }, status: :unprocessable_entity
+      end
+    end
+  
+    def destroy
+      @sneaker.destroy
+      head :no_content
+    end
+  
+    private
+  
+    def set_sneaker
+      @sneaker = Sneaker.find(params[:id])
+    end
+  
+    def sneaker_params
+      params.require(:sneaker).permit(:brand, :model, :size, :condition, :retail_price)
+    end
+  end
+  
