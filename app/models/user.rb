d6c99dffb7a3da404e36e632e09f6b873cd84587
jakebroffman <jake.broffman1088@gmail.com>
2023-11-28 14:46:04 -0500

    update User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
-end
+    has_many :reviews
+    has_many :sneakers, through: :reviews
+  
+    validates :profile_photo_url, format: { with: URI::DEFAULT_PARSER.make_regexp, message: 'is not a valid URL' }, allow_blank: true
+  end
