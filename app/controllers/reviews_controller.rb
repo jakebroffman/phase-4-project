e7e01adc375abfe93b7e7e87e7ed6d5874ad1206
jakebroffman <jake.broffman1088@gmail.com>
2023-11-28 19:10:20 -0500

    build basic controller actions for ReviewsController

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,38 @@
 class ReviewsController < ApplicationController
-end
+    before_action :set_sneaker
+    before_action :set_review, only: [:show, :edit, :update, :destroy]
+  
+    def index
+      @reviews = @sneaker.reviews
+      render json: @reviews
+    end
+  
+    def new
+      @review = @sneaker.reviews.build
+      render json: @review
+    end
+  
+    def create
+      @review = @sneaker.reviews.build(review_params)
+      if @review.save
+        render json: @review, status: :created
+      else
+        render json: { errors: @review.errors.full_messages }, status: :unprocessable_entity
+      end
+    end
+  
+    private
+  
+    def set_sneaker
+      @sneaker = Sneaker.find(params[:sneaker_id])
+    end
+  
+    def set_review
+      @review = @sneaker.reviews.find(params[:id])
+    end
+  
+    def review_params
+      params.require(:review).permit(:user_id, :rating, :comment)
+    end
+  end
+  
