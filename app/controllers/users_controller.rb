74a6db760b5afd5d87e749490f666468f97b22f3
jakebroffman <jake.broffman1088@gmail.com>
2023-12-03 17:44:38 -0500

    update user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -37,7 +37,7 @@ class UsersController < ApplicationController
     private
   
     def set_user
-      @user = User.find_by(id: session[:user_id])
+      @user = User.find(id: session[:user_id])
     end
   
     def user_params
