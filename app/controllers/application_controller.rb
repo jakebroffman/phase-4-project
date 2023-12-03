3e9479ed389f8f8946f29bb34dc1e5f8206942d7
jakebroffman <jake.broffman1088@gmail.com>
2023-12-03 17:32:49 -0500

    build authenticate user method in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
-class ApplicationController < ActionController::Base
-end
+class ApplicationController < ActionController::API
+    before_action :authenticate_user!
+  
+    private
+  
+    def authenticate_user!
+      render json: { error: 'Unauthorized' }, status: :unauthorized unless current_user
+    end
+  
+    def current_user
+      @current_user ||= User.find_by(id: session[:user_id])
+    end
+  end
+  
