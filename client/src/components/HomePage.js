d7f9cac15582ab3085512b2b7075b4705207d0e1
jakebroffman <jake.broffman1088@gmail.com>
2023-12-05 16:33:04 -0500

    build basic HomePage component

---
+++ client/src/components/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function HomePage() {
+  return (
+    <div>
+      <h2>Welcome to Sneaker World!</h2>
+      <p>Discover the latest and coolest sneakers in the market.</p>
+    </div>
+  );
+}
+
+export default HomePage;
