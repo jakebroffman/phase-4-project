5b03d616d250d4c5cb484d4f9f7c497fc434c8e3
jakebroffman <jake.broffman1088@gmail.com>
2023-12-09 02:55:50 -0500

    debug sneakerdetail route

--- client/src/App.js
+++ client/src/App.js
@@ -4,16 +4,22 @@ import NavBar from './components/NavBar';
 import HomePage from './components/HomePage';
 import SneakerPage from './components/SneakerPage';
 import SignUpPage from './components/SignUpPage';
+import SignInPage from "./components/SignInPage";
+import SneakerDetail from "./components/SneakerDetail"
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+
   return (
     <Router>
       <div>
         <NavBar />
         <Routes>
           <Route path="/" element={<HomePage/>} />
-          <Route path="/sneakers" element={<SneakerPage/>} />
+          <Route path="/sneakers" element={<SneakerPage isLoggedIn = {isLoggedIn}/>} />
           <Route path="/signup" element={<SignUpPage/>} />
+          <Route path="/signin" element={<SignInPage/>} />
+          <Route path="/sneakers/:id" element={<SneakerDetail/>} />
         </Routes>
       </div>
     </Router>
