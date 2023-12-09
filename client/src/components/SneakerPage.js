04875d0ec1529e97a531a690fd0023be49724268
jakebroffman <jake.broffman1088@gmail.com>
2023-12-09 02:58:26 -0500

    update sneakerpage component

--- client/src/components/SneakerPage.js
+++ client/src/components/SneakerPage.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 function SneakerPage() {
   const [sneakers, setSneakers] = useState([]);
@@ -10,19 +11,21 @@ function SneakerPage() {
   }, []);
 
   return (
-    <div className="sneaker-page">
-        <h2>All Sneakers</h2>
-            <div className="sneaker-list">
-                {sneakers.map((sneaker) => (
-                    <div key={sneaker.id} className="sneaker-card">
-                        <h3>{sneaker.brand} - {sneaker.model}</h3>
-                        <p>Size: {sneaker.size}</p>
-                        <p>Condition: {sneaker.condition}</p>
-                        <img src={sneaker.photo_url} alt={`${sneaker.brand} ${sneaker.model}`} />
-                    </div>
-                ))}
-            </div>
-        </div>
-    );
+    <div>
+      <h2>All Sneakers</h2>
+      <div className="sneaker-list">
+        {sneakers.map((sneaker) => (
+          <Link key={sneaker.id} to={`/sneakers/${sneaker.id}`} className="sneaker-card">
+            <h3>{sneaker.brand} - {sneaker.model}</h3>
+            <p>Size: {sneaker.size}</p>
+            <p>Condition: {sneaker.condition}</p>
+            <img src={sneaker.photo_url} alt={`${sneaker.brand} ${sneaker.model}`} />
+          </Link>
+        ))}
+      </div>
+    </div>
+);
 }
+
 export default SneakerPage;
+
