b02d93dff4e92df25d80b3d37dd1738163f9961c
jakebroffman <jake.broffman1088@gmail.com>
2023-12-30 18:15:39 -0500

    refactor controller

--- app/controllers/sneakers_controller.rb
+++ app/controllers/sneakers_controller.rb
@@ -1,54 +1,61 @@
 class SneakersController < ApplicationController
-    before_action :set_sneaker, only: [:show, :update, :destroy, :custom_show]
-    before_action :authenticate_user!, only: [:destroy]
+  before_action :set_sneaker, only: [:show, :update, :destroy, :custom_show]
+  before_action :authenticate_user!, only: [:destroy]
 
-    def index
-      @sneakers = Sneaker.all
-      render json: @sneakers
-    end
-  
-    def show
-      byebug
-      @sneaker = Sneaker.includes(reviews: :user).find(params[:id])
-      render json: @sneaker, include: { reviews: {include: :user} }
-    end
+  def index
+    @sneakers = Sneaker.all
+    render json: @sneakers
+  end
 
-    def custom_show
-      render json: @sneaker, include: :reviews
-    end
+  def show
+    render_sneaker_with_reviews
+  end
 
-    def create
-      @sneaker = Sneaker.new(sneaker_params)
-  
-      if @sneaker.save
-        render json: @sneaker, status: :created
-      else
-        render json: { errors: @sneaker.errors.full_messages }, status: :unprocessable_entity
-      end
-    end
-  
-    def update
-      if @sneaker.update(sneaker_params)
-        render json: @sneaker
-      else
-        render json: { errors: @sneaker.errors.full_messages }, status: :unprocessable_entity
-      end
-    end
-  
-    def destroy
-      @sneaker.destroy
-      head :no_content
-    end
-  
-    private
-  
-    def set_sneaker
-      @sneaker = Sneaker.find(params[:id])
-    end
-  
-    def sneaker_params
-      params.require(:sneaker).permit(:brand, :model, :size, :condition, :retail_price, :photo_url)
+  def custom_show
+    render_sneaker_with_reviews
+  end
+
+  def create
+    @sneaker = current_user.sneakers.build(sneaker_params)
+
+    if save_and_render_response(@sneaker)
+      render json: @sneaker, status: :created
     end
   end
-  
-  
+
+  def update
+    if save_and_render_response(@sneaker)
+      render json: @sneaker
+    end
+  end
+
+  def destroy
+    @sneaker.destroy
+    @sneaker.reviews.destroy_all
+    head :no_content
+  end
+
+  private
+
+  def set_sneaker
+    @sneaker = Sneaker.find(params[:id])
+  end
+
+  def sneaker_params
+    params.require(:sneaker).permit(:brand, :model, :size, :condition, :retail_price, :photo_url, :user_id)
+  end
+
+  def render_sneaker_with_reviews
+    @sneaker = Sneaker.includes(reviews: :user).find(params[:id])
+    render json: @sneaker, include: { reviews: { include: :user } }
+  end
+
+  def save_and_render_response(sneaker)
+    if sneaker.save
+      true
+    else
+      render json: { errors: sneaker.errors.full_messages }, status: :unprocessable_entity
+      false
+    end
+  end
+end
