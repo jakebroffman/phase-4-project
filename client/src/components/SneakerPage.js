b8293d96dd2a35454bdf0f6327fbec20e2dc3979
jakebroffman <jake.broffman1088@gmail.com>
2023-12-30 20:07:33 -0500

    Add edit/delete functionality to sneakers

--- client/src/components/SneakerPage.js
+++ client/src/components/SneakerPage.js
@@ -1,35 +1,146 @@
 import React, { useState, useContext } from 'react';
 import { Link } from 'react-router-dom';
-import SneakerContext from './SneakersContext';
+import SneakersContext from './SneakersContext';
 import SneakerForm from './SneakerForm';
 import UserContext from './UserContext';
 
 function SneakerPage() {
-  const { sneakers } = useContext(SneakerContext);
-  const { isLoggedIn } = useContext(UserContext);
+  const { sneakers, setSneakers } = useContext(SneakersContext);
+  const { isLoggedIn, currentUser } = useContext(UserContext);
   const [isFormVisible, setIsFormVisible] = useState(false);
+  const [isEditFormVisible, setIsEditFormVisible] = useState(false);
+  const [sneakerToEdit, setSneakerToEdit] = useState(null);
+
+  const toggleEditForm = (sneaker) => {
+    setIsFormVisible(false);
+    setIsEditFormVisible(!isEditFormVisible);
+    setSneakerToEdit(sneaker);
+  };
+
+  const handleEditSneaker = async (editedSneaker) => {
+    try {
+      const response = await fetch(`/sneakers/${editedSneaker.id}`, {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(editedSneaker),
+      });
+
+      if (response.ok) {
+        const updatedSneaker = await response.json();
+
+        setSneakers((prevSneakers) =>
+          prevSneakers.map((sneaker) =>
+            sneaker.id === updatedSneaker.id ? updatedSneaker : sneaker
+          )
+        );
+
+        
+        console.log('Sneaker updated successfully!');
+      } else {
+        console.error('Failed to update sneaker');
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
+  const handleCreateSneaker = async (newSneaker) => {
+    try {
+      const response = await fetch('/sneakers', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          ...newSneaker,
+          user_id: currentUser.id,
+        }),
+      });
+
+      if (response.ok) {
+        const createdSneaker = await response.json();
+        setSneakers((prevSneakers) => [...prevSneakers, createdSneaker]);
+        setIsFormVisible(false);
+        setIsEditFormVisible(!isEditFormVisible);
+        console.log('Sneaker added successfully!');
+      } else {
+        console.error('Failed to add sneaker to the database');
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
+  const handleDeleteSneaker = async (sneakerId) => {
+    try {
+      const response = await fetch(`/sneakers/${sneakerId}`, {
+        method: 'DELETE',
+      });
+
+      if (response.ok) {
+        setSneakers((prevSneakers) =>
+          prevSneakers.filter((sneaker) => sneaker.id !== sneakerId)
+        );
+
+        console.log('Sneaker deleted successfully!');
+      } else {
+        console.error('Failed to delete sneaker');
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
 
   return (
     <div>
       <h2>All Sneakers</h2>
       {isLoggedIn && (
-        <button onClick={() => setIsFormVisible(!isFormVisible)}>
-          Add Sneaker to the Database
-        </button>
+        <>
+          <button onClick={() => setIsFormVisible(!isFormVisible)}>
+            Add Sneaker to the Database
+          </button>
+          {isFormVisible && (
+            <SneakerForm
+              onSubmit={handleCreateSneaker}
+              isEditFormVisible={isEditFormVisible}
+              setIsEditFormVisible={setIsEditFormVisible}
+              isFormVisible={isFormVisible}
+              setIsFormVisible={setIsFormVisible}
+            />
+          )}
+        </>
       )}
-
-      {isFormVisible && <SneakerForm />}
 
       <div className="sneaker-list">
         {sneakers.map((sneaker) => (
-          <Link key={sneaker.id} to={`/sneakers/${sneaker.id}`} className="sneaker-card">
-            <h3>{sneaker.brand} - {sneaker.model}</h3>
-            <p>Size: {sneaker.size}</p>
-            <p>Condition: {sneaker.condition}</p>
-            <img src={sneaker.photo_url} alt={`${sneaker.brand} ${sneaker.model}`} />
-          </Link>
+          <div key={sneaker.id} className="sneaker-card">
+            <Link to={`/sneakers/${sneaker.id}`}>
+              <h3>
+                {sneaker.brand} - {sneaker.model}
+              </h3>
+              <p>Size: {sneaker.size}</p>
+              <p>Condition: {sneaker.condition}</p>
+              <img src={sneaker.photo_url} alt={`${sneaker.brand} ${sneaker.model}`} />
+            </Link>
+            {isLoggedIn && currentUser.id === sneaker.user_id && (
+              <div>
+                <button onClick={() => toggleEditForm(sneaker)}>Edit</button>
+                <button onClick={() => handleDeleteSneaker(sneaker.id)}>Delete</button>
+              </div>
+            )}
+          </div>
         ))}
       </div>
+
+      {isEditFormVisible && (
+        <SneakerForm
+          onSubmit={handleEditSneaker}
+          sneakerToEdit={sneakerToEdit}
+          onCancel={() => setIsEditFormVisible(false)}
+        />
+      )}
     </div>
   );
 }
